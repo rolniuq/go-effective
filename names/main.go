abd1b97b9a7c4592edcff87feece8214954d71f8
rolnuiqdev <truongquynh2525@gmail.com>
2024-10-09 21:54:35 +0700

    chore: names

---
+++ names/main.go
@@ -0,0 +1,13 @@
+package main
+
+// if start by lowercase -> struct, interface, func just use inside package
+type insidePackage struct {
+}
+
+// if start by uppercase -> struct, interface, func can use outside package
+type OutsidePackage struct {
+}
+
+func main() {
+
+}
