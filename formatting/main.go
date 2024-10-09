1a8525083a799f7fce48607cf2f0ca6a60e736e2
rolnuiqdev <truongquynh2525@gmail.com>
2024-10-09 21:51:32 +0700

    chore: formatting

---
+++ formatting/main.go
@@ -0,0 +1,11 @@
+package main
+
+// All Go code in the standard packages has been formatted with gofmt.
+type T struct {
+	name  string // name of object
+	value int    // its value
+}
+
+func main() {
+
+}
